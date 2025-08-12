222d77c62f18cba7a0a677ac518b32ed68c84295
Lucca Gomes Barreto (Frimesa) <lbarreto@frimesa.com.br>
2025-08-12 15:31:17 -0300

    front updated, features of todo done

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+# definindo modelo de dados
+class Tasks(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    description = db.Column(db.String(100), unique=True, nullable=False)
+
+
+@app.route('/')
+def root():
+    return '<h1>pagina inicial</h1>'
+
+
+
+# cRud - read
+@app.route('/home')
+def index():
+    tasks = Tasks.query.all()
+    return render_template('index.html', tasks=tasks)
+
+# Crud
+@app.route('/create', methods=["POST"])
+def create_task():
+    description = request.form['description']
+
+    # valida se a task ja foi cadastrada
+    exists_task = Tasks.query.filter_by(description = description).first()
+    if(exists_task):
+        return 'Error: repeted task.', 400
+
+    new_task = Tasks(description = description)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect('/')
+
+# cruD
+@app.route('/delete/<int:task_id>', methods=['POST'])
+def delete_task(task_id):
+    task = Tasks.query.get(task_id)
+
+    if task:
+        db.session.delete(task)
+        db.session.commit()
+    return redirect('/')
+
+
+# crUd
+@app.route('/update/<int:task_id>', methods=['POST'])
+def update_task(task_id):
+    task = Tasks.query.get(task_id)
+
+    if task:
+        task.description = request.form['description']
+        db.session.commit()
+    return redirect('/')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True, port=5153)
